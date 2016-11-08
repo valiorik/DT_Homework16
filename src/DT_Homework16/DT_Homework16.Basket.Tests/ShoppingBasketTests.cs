1d511c289cc71fd1fd2bb8216e0f35ec969a97e5
valiorik <valiorik@gmail.com>
2016-11-08 21:36:19 +0000

    Added first test for basket

---
+++ src/DT_Homework16/DT_Homework16.Basket.Tests/ShoppingBasketTests.cs
@@ -0,0 +1,41 @@
+﻿using DT_Homework16.Data;
+using DT_Homework16.Data.Contracts;
+using NUnit.Framework;
+using System.Linq;
+
+namespace DT_Homework16.Basket.Tests
+{
+    [TestFixture]
+    public class ShoppingBasketTests
+    {
+        private Item bread;
+        private Item milk;
+        private Item butter;
+
+        [SetUp]
+        public void setUp()
+        {
+            var dataStore = new StaticItemsSource();
+            var allItems = dataStore.GetAllItems();
+
+            bread = allItems.Single(i => i.Name == "Bread");
+            milk = allItems.Single(i => i.Name == "Milk");
+            butter = allItems.Single(i => i.Name == "Butter");
+        }
+
+        [Test]
+        public void BasketHas1Bread1ButterAnd1Milk()
+        {
+            IShoppingBasket basket = new ShoppingBasket();
+
+            basket.AddItem(bread);
+            basket.AddItem(bread);
+            basket.AddItem(bread);
+
+            var total = basket.GetTotalPrice();
+
+            Assert.That(total, Is.EqualTo(2.95));
+
+        }
+    }
+}
---
+++ src/DT_Homework16/DT_Homework16.Basket/ShoppingBasket.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using DT_Homework16.Data.Contracts;
+
+namespace DT_Homework16.Basket
+{
+    public class ShoppingBasket : IShoppingBasket
+    {
+        public void AddItem(Item item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public double GetTotalPrice()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
