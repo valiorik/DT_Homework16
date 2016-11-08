604863e5ed8510793a2b9547a410ab88b8fc9151
valiorik <valiorik@gmail.com>
2016-11-08 21:54:40 +0000

    Implemented simple basket calculation

--- src/DT_Homework16/DT_Homework16.Basket/ShoppingBasket.cs
+++ src/DT_Homework16/DT_Homework16.Basket/ShoppingBasket.cs
@@ -1,18 +1,26 @@
-﻿using System;
-using DT_Homework16.Data.Contracts;
+﻿using DT_Homework16.Data.Contracts;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace DT_Homework16.Basket
 {
     public class ShoppingBasket : IShoppingBasket
     {
+        List<Item> itemsInBasket;
+
+        public ShoppingBasket()
+        {
+            itemsInBasket = new List<Item>();
+        }
+
         public void AddItem(Item item)
         {
-            throw new NotImplementedException();
+            itemsInBasket.Add(item);
         }
 
         public double GetTotalPrice()
         {
-            throw new NotImplementedException();
+            return itemsInBasket.Sum(i => i.Price);
         }
     }
 }
