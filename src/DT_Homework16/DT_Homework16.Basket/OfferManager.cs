8efbe9229bf4eb0e5ecefbfd14614b1e9738bfc4
valiorik <valiorik@gmail.com>
2016-11-09 09:14:48 +0000

    Refactored ShoppingBasket class

---
+++ src/DT_Homework16/DT_Homework16.Basket/OfferManager.cs
@@ -0,0 +1,52 @@
+﻿using DT_Homework16.Data.Contracts;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DT_Homework16.Basket
+{
+    internal class OfferManager
+    {
+        private IEnumerable<Offer> offers;
+        
+        public OfferManager(IEnumerable<Offer> offers)
+        {
+            this.offers = offers;
+        }
+
+        public void ApplyOffersForItems(List<BasketItem> items)
+        {
+            // Search for offers
+            foreach (var offer in offers)
+            {
+                // if offer items are present in basket and enougth quantity
+
+                while (items.Where(i => i.Item.Name == offer.TargetProduct && i.AvailableForDiscount).Count() >= offer.TargetProductQuantity)
+                {
+                    // Mark those items as used for a discount
+                    for (int index = 0; index < offer.TargetProductQuantity; index++)
+                    {
+                        items.First(i => i.Item.Name == offer.TargetProduct && i.AvailableForDiscount).MarkAsUsedForOffer();
+                    }
+
+                    ApplyDiscount(items, offer.Discounts);
+                }
+            }
+        }
+
+        private void ApplyDiscount(List<BasketItem> items, IEnumerable<Discount> discounts)
+        {
+            // Apply discounts from the offer
+            foreach (var discount in discounts)
+            {
+                // Look for item in basket
+                var availableItemForDiscount = items.FirstOrDefault(i => i.AvailableForDiscount && i.Item.Name == discount.DiscountedProduct);
+
+                if (availableItemForDiscount != null)
+                {
+                    // If item was found apply discount to it
+                    availableItemForDiscount.ApplyDiscount(discount.DiscountPercentage);
+                }
+            }
+        }
+    }
+}
--- src/DT_Homework16/DT_Homework16.Basket/ShoppingBasket.cs
+++ src/DT_Homework16/DT_Homework16.Basket/ShoppingBasket.cs
@@ -7,13 +7,13 @@ namespace DT_Homework16.Basket
 {
     public class ShoppingBasket : IShoppingBasket
     {
-        List<BasketItem> itemsInBasket;
-        IEnumerable<Offer> offers;
+        private List<BasketItem> itemsInBasket;
+        private OfferManager offerManager;
 
         public ShoppingBasket(IEnumerable<Offer> offers)
         {
             itemsInBasket = new List<BasketItem>();
-            this.offers = offers;
+            offerManager = new OfferManager(offers);
         }
 
         public void AddItem(Item item)
@@ -25,33 +25,7 @@ namespace DT_Homework16.Basket
         {
             double total = 0.0;
 
-            // Search for offers
-            foreach (var offer in offers)
-            {
-                // if offer items are present in basket and enougth quantity
-
-                while (itemsInBasket.Where(i => i.Item.Name == offer.TargetProduct && i.AvailableForDiscount).Count() >= offer.TargetProductQuantity)
-                {
-                    // Mark those items as used for a discount
-                    for (int index = 0; index < offer.TargetProductQuantity; index++)
-                    {
-                        itemsInBasket.First(i => i.Item.Name == offer.TargetProduct && i.AvailableForDiscount).MarkAsUsedForOffer();
-                    }
-
-                    // Now apply discounts from the offer
-                    foreach (var discount in offer.Discounts)
-                    {
-                        // Look for item in basket
-                        var availableItemForDiscount = itemsInBasket.FirstOrDefault(i => i.AvailableForDiscount && i.Item.Name == discount.DiscountedProduct);
-                        
-                        if (availableItemForDiscount != null)
-                        {
-                            // If item was found apply discount to it
-                            availableItemForDiscount.ApplyDiscount(discount.DiscountPercentage);
-                        }
-                    }
-                } 
-            }
+            offerManager.ApplyOffersForItems(itemsInBasket);
 
             total = itemsInBasket.Sum(i => i.FinalPrice);
             return Math.Round(total, 2);
